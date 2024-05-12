9934da1de1c98ea29ca71cd2602b58a0a2c82daa
N098I765H432L1lus <99386829+N098I765H432L1lus@users.noreply.github.com>
2024-05-12 18:31:28 +0400

    Add files via upload

--- controller/dell_dev.php
+++ controller/dell_dev.php
@@ -4,7 +4,7 @@ include_once 'db.php';
 $id=$_GET['id'];
 
 $del_sql = "DELETE FROM `technic` WHERE `id` = '$id'";
-$run_del_sql = mysqli_query($con, $del_sql);
+//$run_del_sql = mysqli_query($con, $del_sql);
 
 //Количество
 //поиск
@@ -12,7 +12,6 @@ $sel_typ_tehn_dev_sql = "SELECT * FROM `technic` WHERE `id`='$id'";
 $run_typ_tehn_dev_sql = mysqli_query($con, $sel_typ_tehn_dev_sql);
 $row_t_d = mysqli_fetch_array($run_typ_tehn_dev_sql);
 $Type_dev = $row_t_d['Type_dev'];
-echo "$Type_dev<br>";
 //Изменение
 $quantity_ply_type_mql="SELECT * FROM `Type_dev` WHERE `id`='$Type_dev'";
 $run_quantity_sql=mysqli_query($con, $quantity_ply_type_mql);
@@ -23,7 +22,11 @@ $description=$row_quantity['description'];
 $quantity_aces=$row_quantity['quantity_aces'];
 $quantity = $row_quantity['quantity'];
 $quantity -= 1;
-$quantity_type_mql="UPDATE `Type_dev` SET `quantity`='$quantity' WHERE `id`='$Type_dev'";
+$quantity_type_mql="UPDATE `Type_dev` SET `quantity` = '$quantity' WHERE `Type_dev`.`id` = '$Type_dev'";
+
+
+//детали
+$upd_aces = "UPDATE `Accessories` SET `id_dev`='3' WHERE `id_dev`='$id'";
 
 if ($id='3') {
 	$_SESSION['error']="Нельзя удалить(Важный-Не редоктируемый)";
@@ -32,6 +35,7 @@ if ($id='3') {
 else {
 if ($run_del_sql) {
 	$run_qua_acec = mysqli_query($con, $quantity_type_mql);
+	$run_upd_aces = mysqli_query($con, $upd_aces);
 	$file="dev.php";
 }
 else {
@@ -39,5 +43,5 @@ else {
 	$file="dev.php";
 }
 }
-//header("Location:/$file");
+header("Location:/$file");
 ?>
